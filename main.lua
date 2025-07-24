3ae941a7edde06613f89903d2214c2533c4bc8ab
Rajman03 <151401107+Rajman03@users.noreply.github.com>
2025-07-24 12:01:00 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -57,29 +57,6 @@ local Button = PlayerTab:CreateButton({
     end,
  })
 
- local Button = PlayerTab:CreateButton({
-   Name = "Infinite Jump",
-   Callback = function()
-           --Toggles the infinite jump between on or off on every script run
-_G.infinjump = not _G.infinjump
-
-if _G.infinJumpStarted == nil then
-	--Ensures this only runs once to save resources
-	_G.infinJumpStarted = true
-
-	--The actual infinite jump
-	local plr = game:GetService('Players').LocalPlayer
-	local m = plr:GetMouse()
-	m.KeyDown:connect(function(k)
-		if _G.infinjump then
-			if k:byte() == 32 then
-			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
-			humanoid:ChangeState('Jumping')
-			wait()
-			humanoid:ChangeState('Seated')
-   end,
-})
-
 local Button = PlayerTab:CreateButton({
    Name = "Tp to Spawn ",
    Callback = function()
